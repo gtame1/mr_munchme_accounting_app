624d6711abc2d2dc089b9519db21df92eb5a26ae
gtame1 <guillotame@gmail.com>
2025-12-02 17:15:43 -0600

    adjust release.ex file

--- lib/mr_munch_me_accounting_app/release.ex
+++ lib/mr_munch_me_accounting_app/release.ex
@@ -1,28 +1,19 @@
 defmodule MrMunchMeAccountingApp.Release do
-  @moduledoc """
-  Helpers for running tasks in the release (like migrations).
-  """
-
   @app :mr_munch_me_accounting_app
 
   def migrate do
-    IO.puts("🧱 Running database migrations...")
+    IO.puts("🚀 Running migrations...")
 
     load_app()
 
     for repo <- repos() do
-      IO.puts("➡ Migrating #{inspect(repo)}")
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
 
-    IO.puts("✅ Migrations finished.")
+    IO.puts("✅ Migrations complete")
   end
 
-  defp repos do
-    Application.fetch_env!(@app, :ecto_repos)
-  end
+  defp repos, do: Application.fetch_env!(@app, :ecto_repos)
 
-  defp load_app do
-    Application.load(@app)
-  end
+  defp load_app, do: Application.load(@app)
 end
