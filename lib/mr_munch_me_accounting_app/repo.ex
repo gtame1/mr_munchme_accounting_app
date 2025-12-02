1264b2ef3c82769b0df773b716e0aab9698203f9
gtame1 <guillotame@gmail.com>
2025-12-02 17:22:04 -0600

    Add logging to Repo

--- lib/mr_munch_me_accounting_app/repo.ex
+++ lib/mr_munch_me_accounting_app/repo.ex
@@ -2,4 +2,11 @@ defmodule MrMunchMeAccountingApp.Repo do
   use Ecto.Repo,
     otp_app: :mr_munch_me_accounting_app,
     adapter: Ecto.Adapters.Postgres
+
+  @impl true
+  def init(_type, config) do
+    require Logger
+    Logger.info("📦 Repo init config: #{inspect(config, pretty: true)}")
+    {:ok, config}
+  end
 end
